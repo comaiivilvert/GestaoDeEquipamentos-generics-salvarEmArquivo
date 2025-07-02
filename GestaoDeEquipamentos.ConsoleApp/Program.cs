2366f83fa7c2d9f3e08c16b4bc6cf0ef2dddda80
comaiivilvert <comaiivilvert@gmail.com>
2025-07-01 21:30:21 -0300

    Add project files.

---
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using GestaoDeEquipamentos.ConsoleApp.Compartilhado;
+
+namespace GestaoDeEquipamentos.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+
+        while (true)
+        {
+            telaPrincipal.ApresentarMenuPrincipal();
+
+            ITela telaEscolhida = telaPrincipal.ObterTela();
+
+            if (telaEscolhida == null)
+                break;
+
+            char opcaoEscolhida = telaEscolhida.ApresentarMenu();
+
+            if (char.ToUpper(opcaoEscolhida) == 'S')
+                break;
+
+            switch (opcaoEscolhida)
+            {
+                case '1':
+                    telaEscolhida.CadastrarRegistro();
+                    break;
+
+                case '2':
+                    telaEscolhida.EditarRegistro();
+                    break;
+
+                case '3':
+                    telaEscolhida.ExcluirRegistro();
+                    break;
+
+                case '4':
+                    telaEscolhida.VisualizarRegistros(true);
+                    break;
+            }
+        }
+    }
+}
