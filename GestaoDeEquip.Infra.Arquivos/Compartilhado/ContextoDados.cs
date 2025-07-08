90b011cd6cd432c98aee821189a0d1f0c8e86e4d
comaiivilvert <comaiivilvert@gmail.com>
2025-07-08 01:34:39 -0300

    implementado repositorioBaseEmArquivo.

---
+++ GestaoDeEquip.Infra.Arquivos/Compartilhado/ContextoDados.cs
@@ -0,0 +1,75 @@
+﻿using GestaoDeEquipamentos.Dominio.ModuloChamado;
+using GestaoDeEquipamentos.Dominio.ModuloEquipamento;
+using GestaoDeEquipamentos.Dominio.ModuloFabricante;
+using System.Runtime;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GestaoDeEquip.Infra.Arquivos.Compartilhado
+{
+    public class ContextoDados
+    {
+        public List<Fabricante> Fabricantes { get; set; }
+        public List<Equipamento> Equipamentos { get; set; }
+        public List<Chamado> Chamado { get; set; }
+
+        private string pastaArmazenamento = "C:\\temp";
+        private string arquivosArmazenamento = "dados.json";
+
+        public ContextoDados(bool carregarDados) : this()
+        {
+            if (carregarDados)
+                Carregar();
+        }
+
+        public ContextoDados() 
+        {
+            Fabricantes = new List<Fabricante>();
+            Equipamentos = new List<Equipamento>();
+            Chamado = new List<Chamado>();
+        }
+
+        public void Salvar()
+        {
+            string caminhoCompleto = Path.Combine(pastaArmazenamento, arquivosArmazenamento);
+
+            JsonSerializerOptions jsonOptions = new JsonSerializerOptions();
+            jsonOptions.WriteIndented = true;
+            jsonOptions.ReferenceHandler = ReferenceHandler.Preserve;
+
+            string conteudoJson = JsonSerializer.Serialize(this, jsonOptions);
+
+            Directory.CreateDirectory(pastaArmazenamento);
+
+            File.WriteAllText(caminhoCompleto, conteudoJson);
+
+        }
+
+        public void Carregar()
+        {
+            string caminhoCompleto = Path.Combine(pastaArmazenamento, arquivosArmazenamento);
+
+            if (!File.Exists(caminhoCompleto))
+                return;
+
+            string conteudoJson = File.ReadAllText(caminhoCompleto);
+
+            if (string.IsNullOrEmpty(caminhoCompleto))
+                return;
+
+            JsonSerializerOptions jsonOptions = new JsonSerializerOptions();
+            jsonOptions.ReferenceHandler = ReferenceHandler.Preserve;
+
+            ContextoDados contextoArmazenado = JsonSerializer.Deserialize<ContextoDados>(conteudoJson, jsonOptions)!;
+
+            if (contextoArmazenado == null) 
+                return;
+            
+            this.Fabricantes = contextoArmazenado.Fabricantes;
+            this.Equipamentos = contextoArmazenado.Equipamentos;
+            this.Chamado = contextoArmazenado.Chamado;
+
+        }
+
+    }
+}
